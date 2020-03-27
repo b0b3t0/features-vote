65c62b4f27b1e0d4120a680a4ed3e46c7de0fb38
Bojidar Nenov <cruelbobo@abv.bg>
2020-03-28 01:31:59 +0200

    Basic entities

---
+++ src/main/java/com/featuresvote/domain/Feature.java
@@ -0,0 +1,53 @@
+package com.featuresvote.domain;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "features")
+public class Feature {
+
+    private Long id;
+    private String tittle;
+    private String description;
+    private String status;
+
+    public Feature() {
+
+    }
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "feature_id", nullable = false, unique = true)
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTittle() {
+        return tittle;
+    }
+
+    public void setTittle(String tittle) {
+        this.tittle = tittle;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
