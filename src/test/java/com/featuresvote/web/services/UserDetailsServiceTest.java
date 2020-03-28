9d1563ab7bd9b483db1a766d48a0783232d7c25e
Bojidar Nenov <cruelbobo@abv.bg>
2020-03-28 21:20:44 +0200

    Adding junit dependency in the pom file and creating testfile to test that user's password is not equal to encoded user's password

---
+++ src/test/java/com/featuresvote/web/services/UserDetailsServiceTest.java
@@ -0,0 +1,19 @@
+package com.featuresvote.web.services;
+
+import org.junit.Test;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import static org.junit.Assert.assertNotEquals;
+
+public class UserDetailsServiceTest {
+
+    @Test
+    public void generate_encrypted_password() {
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        String rawPassword = "password123";
+        String encodedPassword = encoder.encode("password123");
+
+        assertNotEquals(rawPassword,encodedPassword);
+    }
+
+}
