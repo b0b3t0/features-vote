1eb58390b7246a3b27816e4fd812e01810e16a5b
Bojidar Nenov <cruelbobo@abv.bg>
2020-04-01 17:24:39 +0300

    Adding handling for productId routing in ProductController

--- src/main/java/com/featuresvote/web/controllers/ProductController.java
+++ src/main/java/com/featuresvote/web/controllers/ProductController.java
@@ -3,13 +3,19 @@ package com.featuresvote.web.controllers;
 import com.featuresvote.domain.Product;
 import com.featuresvote.domain.User;
 import com.featuresvote.repositories.ProductRepository;
+import javassist.NotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Optional;
 
 @Controller
 public class ProductController {
@@ -28,7 +34,16 @@ public class ProductController {
     }
 
     @GetMapping("/products/{productId}")
-    public String getProduct(@PathVariable Long productId) {
+    public String getProduct(@PathVariable Long productId, ModelMap model, HttpServletResponse response) throws IOException {
+        Optional<Product> productOpt = productRepository.findById(productId);
+        if (productOpt.isPresent()) {
+            Product product = productOpt.get();
+            model.put("product", product);
+        } else {
+            response.sendError(HttpStatus.NOT_FOUND.value(), "Product with id " + productId + " is not found");
+            return "product";
+        }
+
         return "product";
     }
 
