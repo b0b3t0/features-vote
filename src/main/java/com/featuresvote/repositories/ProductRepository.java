c49009ef21a57d0bc531c58e8f3acb0212a50236
Bojidar Nenov <cruelbobo@abv.bg>
2020-04-01 16:51:38 +0300

    Creating ProductRepository to be able to save Products in our database

---
+++ src/main/java/com/featuresvote/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.featuresvote.repositories;
+
+import com.featuresvote.domain.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    
+}
