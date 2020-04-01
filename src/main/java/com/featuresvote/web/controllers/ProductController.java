46670c7cfc634a626a6162bdb3d6f01df5e575b6
Bojidar Nenov <cruelbobo@abv.bg>
2020-04-01 16:59:01 +0300

    Creating PostMapping in ProductController to be able to save product in database and redirect us to productId page

--- src/main/java/com/featuresvote/web/controllers/ProductController.java
+++ src/main/java/com/featuresvote/web/controllers/ProductController.java
@@ -1,21 +1,45 @@
 package com.featuresvote.web.controllers;
 
+import com.featuresvote.domain.Product;
+import com.featuresvote.domain.User;
+import com.featuresvote.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ProductController {
+
+    private final ProductRepository productRepository;
+
+    @Autowired
+    public ProductController(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
 
     @GetMapping("/products")
     public String getProducts(ModelMap model) {
         return "product";
     }
 
-    @PostMapping("/products")
-    public String createProduct(){
+    @GetMapping("/products/{productId}")
+    public String getProduct(@PathVariable Long productId) {
+        return "product";
+    }
 
-        return "redirect:/products";
+    @PostMapping("/products")
+    public String createProduct(@AuthenticationPrincipal User user){
+        Product product = new Product();
+        product.setPublished(false);
+        product.setUser(user);
+
+        product = productRepository.save(product);
+
+        return "redirect:/products/" + product.getId();
     }
 }
