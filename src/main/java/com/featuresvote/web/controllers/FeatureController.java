aaa070cb88996578ef944d6c8fc5838137b088a5
Bojidar Nenov <cruelbobo@abv.bg>
2020-04-12 01:12:23 +0300

    Fix type mistake in FeatureControl requestmapping string

--- src/main/java/com/featuresvote/web/controllers/FeatureController.java
+++ src/main/java/com/featuresvote/web/controllers/FeatureController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/products/{productId}/featers")
+@RequestMapping("/products/{productId}/features")
 public class FeatureController {
 
     private final FeatureService featureService;
